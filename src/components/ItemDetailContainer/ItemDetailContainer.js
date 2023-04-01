3358e2bc18be45ca681372d75d1b6f9dc94e31b6
Gerardo Biancardi <gerabiancardi@gmail.com>
2023-04-01 17:43:54 -0300

    Firebase

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -2,35 +2,31 @@ import React, { useEffect, useState } from "react";
 import ItemDetail from "../ItemDetail/ItemDetail";
 import { useParams } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import {db} from "../../firebase/Firebase"
-import { collection, getDocs} from 'firebase/firestore'
+import { db } from "../../firebase/Firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const ItemDetailContainer = () => {
-  const [detalleProducto, setDetalleProducto] = useState([]);
+  const [detalleProducto, setDetalleProducto] = useState({});
   const { id } = useParams();
 
-
   useEffect(() => {
-    const getProducts = async ()=>{
-      try{
-          const querySnapshot= await getDocs (collection (db, "Items"))
-          const docs= []
-          querySnapshot.forEach((doc)=>{
-              docs.push({...doc.data(), id:doc.id})
-          })
-            setDetalleProducto(docs.filter((doc) => doc.id === id))
-      }catch(error){
-          console.log(error)
+    const getProducts = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Items"));
+        const docs = [];
+        querySnapshot.forEach((doc) => {
+          docs.push({ ...doc.data(), id: doc.id });
+        })(setDetalleProducto(docs.find((doc) => doc.id === id)));
+      } catch (error) {
+        console.log(error);
       }
-  }
-  
-  getProducts()
-  
-  },[detalleProducto])
+    };
+    getProducts();
+  }, [id]);
 
   return (
     <Container>
-        <ItemDetail detalleProducto={detalleProducto} />
+      <ItemDetail detalleProducto={detalleProducto} />
     </Container>
   );
 };
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -3,38 +3,34 @@ import Container from "react-bootstrap/Container";
 import "./ItemListContainer.css";
 import ItemList from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
-import {db} from "../../firebase/Firebase"
-import { collection, getDocs} from 'firebase/firestore'
-
+import { db } from "../../firebase/Firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 function ItemListContainer() {
   const [listaProductos, setListaProductos] = useState([]);
   const { categoryId } = useParams();
- 
- 
+
   useEffect(() => {
-    const getProducts = async ()=>{
-      try{
-          const querySnapshot= await getDocs (collection (db, "Items"))
-          const docs= []
-          querySnapshot.forEach((doc)=>{
-              docs.push({...doc.data(), id:doc.id})
-          })
-          if(categoryId){
-            setListaProductos(docs.filter((doc) => doc.category === categoryId))
-          }else{
-            setListaProductos(docs);
-          }
-          
-      }catch(error){
-          console.log(error)
+    const getProducts = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Items"));
+        const docs = [];
+        querySnapshot.forEach((doc) => {
+          docs.push({ ...doc.data(), id: doc.id });
+        });
+        if (categoryId) {
+          setListaProductos(docs.filter((doc) => doc.category === categoryId));
+        } else {
+          setListaProductos(docs);
+        }
+      } catch (error) {
+        console.log(error);
       }
-  }
-  
-  getProducts()
-  
-  },[listaProductos])
-  
+    };
+
+    getProducts();
+  }, [categoryId]);
+
   return (
     <Container>
       <ItemList productos={listaProductos} />
